3a592c0df57d39e0bee691a8cd2f447e0644d179
mangmuse <mangmangmuse@gmail.com>
2024-05-13 20:19:47 +0900

    assignment done

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,54 @@ function App() {
     { id: 1, name: "John", age: 20 },
     { id: 2, name: "Doe", age: 21 },
   ];
-  const [users, setUsers] = useState(initialState);
+  const initialUserInfo = { name: "", age: "" };
 
-  // TODO: 이름과 나이를 각각 상태로 정의하세요. 초기값은 빈문자열("")입니다.
+  const [users, setUsers] = useState(initialState);
+  const [userInfo, setUserInfo] = useState(initialUserInfo);
+  const handleChange = (e, key) =>
+    setUserInfo((prev) => ({ ...prev, [key]: e.target.value }));
 
   const addUser = (e) => {
     e.preventDefault();
-    // TODO: 이름과 나이가 모두 입력되지 않았을 때는 alert 처리하고 함수를 종료하세요. 논리합연산자 (||) 를 이용하세요.
-
-    // TODO: 사용자 리스트 상태를 업데이트 하세요. spread operator 를 사용하고, 추가되는 id는 현재 시간을 밀리초 단위로 반환하는 Date.now() 를 사용하세요.
+    if (userInfo.name.trim() === "" || userInfo.age.trim() === "") {
+      alert("asd");
+      return;
+    }
+    const { name, age } = userInfo;
+    setUsers((prev) => [...prev, { id: Date.now(), name, age }]);
+    setUserInfo(initialUserInfo);
   };
 
-  const removeUser = (id) => {
-    // TODO: filter 메소드를 사용해서 사용자 삭제 로직을 구현해 보세요.
+  const handleDelete = (id) => {
+    setUsers((users) => users.filter((user) => user.id !== id));
   };
 
   return (
     <>
       <h1>사용자 리스트</h1>
       <form onSubmit={addUser}>
-        {/* TODO: input 태그에 value, onChange 속성을 추가해서 이름과 나이의 상태와 상태변경 로직을 연결하세요 */}
-        <input type="text" placeholder="이름" />
-        <input type="number" placeholder="나이" />
+        <input
+          onChange={(e) => handleChange(e, "name")}
+          type="text"
+          placeholder="이름"
+          value={userInfo.name}
+        />
+        <input
+          onChange={(e) => handleChange(e, "age")}
+          type="number"
+          placeholder="나이"
+          value={userInfo.age}
+        />
         <button type="submit">사용자 추가</button>
       </form>
       <ul>
-        {/* TODO: map 메소드를 이용해서 user 리스트를 렌더링하세요.  */}
-        {/* 이름: John, 나이: 20 [삭제] 버튼이 하나의 행에 나올 수 있도록 해보세요. (hint: flex) */}
+        {users.map((user) => (
+          <li key={user.id}>
+            <span>{`이름: ${user.name}`}</span>
+            <span>{`나이: ${user.age}`}</span>
+            <button onClick={() => handleDelete(user.id)}>삭제</button>
+          </li>
+        ))}
       </ul>
     </>
   );
